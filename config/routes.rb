77afc97a806c92e612d8a1dad86eb8c0cd9b89c4
Diploma <nguyendhoan9@gmail.com>
2019-06-25 22:32:07 +0700

    Finish statis page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+	root 'static_page#home'
+
   get 'static_page/home'
 
   get 'static_page/help'
 
+  get 'static_page/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
