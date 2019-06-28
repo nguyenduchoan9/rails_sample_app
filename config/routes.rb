a846cc64f0abf856ac703aefb9204b51c7a6d172
Diploma <nguyendhoan9@gmail.com>
2019-06-28 23:40:23 +0700

    Styling Css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  	
 	root 'static_page#home'
-
-  get 'static_page/home'
-
-  get 'static_page/help'
-
-  get 'static_page/about'
+  	get  '/help',    to: 'static_page#help'
+  	get  '/about',   to: 'static_page#about'
+  	get  '/contact', to: 'static_page#contact'
+  	get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
